d927059d4543bdef1fd0c32486588810eb2a3453
annaloginova <a.vik00@mail.ru>
2022-09-08 16:37:12 +0300

    dz2_3

---
+++ dz2_z3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("выходной день");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("нет такого дня недели");
+    }
+    else Console.WriteLine("невыходной день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
