ceaf452cf2abd7fe9dd921e5b0aca24ac7dc16be
annaloginova <a.vik00@mail.ru>
2022-09-08 15:45:44 +0300

    dz2_z1

---
+++ dz2_z1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(stringNumber[1]);
